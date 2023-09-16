11b7ba4e52bc7e2c88ddae7d87d869df1ee2816d
frantic-dev <okeyamazing@gmail.com>
2023-09-16 20:35:54 +0100

    4.14 Blog list expansions, step2 finished

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,4 +19,16 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    content: body.content,
+    important: body.important,
+  }
+
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(blog)
+})
+
 module.exports = blogsRouter
